10fc6a81cb7c52b64d30d354aa04c170c2f45d51
HackeZ <767110505@qq.com>
2016-09-05 13:22:53 +0800

    update SHA1 Check

--- FileSHACount/FileSHA1Count_test.go
+++ FileSHACount/FileSHA1Count_test.go
@@ -1,6 +1,9 @@
 package FileSHACount
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestFileSHA1Count(t *testing.T) {
 	// init Params
@@ -13,21 +16,21 @@ func TestFileSHA1Count(t *testing.T) {
 		t.Fatal("Result Length not Corrent!")
 	}
 
+	// init testMap
+	testMap := make(map[string]*os.FileInfo)
+	testMap["19cf1e53712298df8cb75ea4b817b50d9a7671c4"] = nil
+	testMap["a653c1dfd892652dfd439eebca90038c198aa5f9"] = nil
+	testMap["89c46b621fe09daf8633b9a91952c40f7dbfc641"] = nil
+	testMap["d80f835b13b0b66e8dbea4384ff5025517478520"] = nil
+	testMap["753636cbe5ded693ce83dbf470a0b563ea4aa0ac"] = nil
+	testMap["82d51cac5de01733455973610709e021adae4fae"] = nil
+	testMap["9ce996c7481105375a22677284121ecaafe4d682"] = nil
+
+	// Check SHA1 Corrent or Not.
 	for k := range resultMap {
-		if k != "19cf1e53712298df8cb75ea4b817b50d9a7671c4" {
-
-		} else if k != "a653c1dfd892652dfd439eebca90038c198aa5f9" {
-
-		} else if k != "89c46b621fe09daf8633b9a91952c40f7dbfc641" {
-
-		} else if k != "d80f835b13b0b66e8dbea4384ff5025517478520" {
-
-		} else if k != "753636cbe5ded693ce83dbf470a0b563ea4aa0ac" {
-
-		} else if k != "82d51cac5de01733455973610709e021adae4fae" {
-
-		} else if k != "9ce996c7481105375a22677284121ecaafe4d682" {
-			t.Fatal("File SHA1 Count Wrong")
+		if _, isExist := testMap[k]; !isExist {
+			t.Log(k)
+			t.Fatal("File SHA1 Count Wrong!")
 		}
 	}
 }
